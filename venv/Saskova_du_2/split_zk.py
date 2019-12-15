a66f34fc4bf6ea2005a585ea345d115c19bd1db5
saskovamarie <57861267+saskovamarie@users.noreply.github.com>
2019-12-15 13:41:19 +0100

    Delete split_zk.py

--- venv/Saskova_du_2/split_zk.py
+++
@@ -1,8 +0,0 @@
-import json, qdtree
-
-# načtení dat geojson ze složky a uložení do proměnné
-with open ("input.geojson","r",encoding="utf-8") as f:
-    data = json.load(f)
-features = data["features"]
-qdtree.building_quadtree(features)
-
