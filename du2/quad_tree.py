4dbbc2f08ad38ecfba28eecf11ac2145ac24018c
Marie Šašková <saskovamarie11@gmail.com>
2019-12-16 22:16:55 +0100

    přidána funkce split_points - id_cluster

--- du2/quad_tree.py
+++ du2/quad_tree.py
@@ -1,9 +1,12 @@
+
+
 def coords (features):
     points =[]
     for point in features:
         coordinates = point['geometry']['coordinates']
         points.append(coordinates)
-        return points
+    print(points)
+    return points
 
 def edges(features):
     print(len(features))
@@ -23,39 +26,45 @@ def edges(features):
     mid_x = ((left + right)/2)
     mid_y = ((bottom + top)/2)
     print(mid_x,mid_y)
-    return mid_x,mid_y
+    bounding_box = [mid_x,mid_y,left,right,bottom,top]
+    return bounding_box
 
-def building_quadtree (features):
-    if len(features) < 5: # konečná podmínka rekurze
-        # změna id_cluster
-        return(features)
-    #features = data["features"]
-    mid_x, mid_y = edges(features)
+def split_points(points,box):
+
     NW = []
     NE = []
     SW = []
     SE = []
-
-
-    points = coords(features)
     for point in points:
-        x = point[0]
-        print(x)
-        y = point[1]
-        print(y)
-        if (x < mid_x) and (y < mid_y):
-            SW.append(coords)
-        if (x < mid_x) and (y > mid_y):
-            NW.append(coords)
-        if (x > mid_x) and (y < mid_y):
-            SE.append(coords)
-        if (x > mid_x) and (y > mid_y):
-            NE.append(coords)
+        point['properties']['id_cluster']=''
+        coordinates = point['geometry']['coordinates']
+        x = coordinates[0]
+        y = coordinates[1]
+        if (x < box[0]) and (y < box[1]):
+            point['properties']['id_cluster'] += '1'
+            SW.append(point)
+        if (x < box[0]) and (y > box[1]):
+            point['properties']['id_cluster'] += '2'
+            NW.append(point)
+        if (x > box[0]) and (y < box[1]):
+            point['properties']['id_cluster'] += '3'
+            SE.append(point)
+        if (x > box[0]) and (y > box[1]):
+            point['properties']['id_cluster'] += '4'
+            NE.append(point)
+    return NW,NE,SW,SE
 
-    building_quadtree(SW)
-    building_quadtree(NW)
-    building_quadtree(SE)
-    building_quadtree(NE)
+def building_quadtree (input_points, box):
+    if len(input_points)< 5:
+        return(input_points)
+
+    NW,NE,SW,SE = split_points(input_points,box)
+
+
+    building_quadtree(SW,box)
+    building_quadtree(NW,box)
+    building_quadtree(SE,box)
+    building_quadtree(NE, box)
     print("sw:",SW, "délka:",len(SW))
     print("nw:",NW,"délka:",len(NW))
     print("se:",SE,"délka:",len(SE))
--- du2/split_zk.py
+++ du2/split_zk.py
@@ -1,10 +1,10 @@
 import json, quad_tree
 
 # načtení dat geojson ze složky a uložení do proměnné
-with open ("input.geojson","r",encoding="utf-8") as f:
+with open ("mrizka_new.json","r",encoding="utf-8") as f:
     data = json.load(f)
 features = data["features"]
 
-
-quad_tree.building_quadtree(features)
+bounding_box = quad_tree.edges(features)
+quad_tree.building_quadtree(features,bounding_box)
 
