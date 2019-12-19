3be76da8ceb14e1e391228b2fbbd3913754a26b2
Marie Šašková <saskovamarie11@gmail.com>
2019-12-19 17:21:49 +0100

    oprava dělení

--- du2/quad_tree.py
+++ du2/quad_tree.py
@@ -1,92 +1,97 @@
 
-def coords (features):
-    """
-    :param features: vstupní seznam prvků vzniklých z geoJSON
-    :return: vrací seznam, který obsahuje souřadnice x,y a prázdné místo dále využité pro cluster_id
-    """
-    points =[]
-    for point in features:
-        x = point['geometry']['coordinates'][0]
-        y = point['geometry']['coordinates'][1]
-        points.append([x,y,''])
-    #print(points)
-    return points
 
 def edges(features):
     """
     :param features: vstupní seznam prvků vzniklých z geoJSON
-    :return points: seznam, který obsahuje souřadnice x,y a prázdné místo dále využité pro cluster_id
     :return bounding box: seznam, který obsahuje souřadnice bodů left,right,bottom a top
     """
-    #print(len(features))
-    # points = data["features"]
-    points = coords(features)
+    points = []
+    for feature in features:
+        coordinates = feature['geometry']['coordinates']
+        points.append(coordinates)
+    print(points)
     # seřazení podle osy X
     pointsX = sorted(points, key=lambda x: x[0])
     left = pointsX[0][0]
     right = pointsX[-1][0]
+    print(left,right)
     # sort podle Y
     pointsY = sorted(points,key=lambda y: y[1])
     bottom = pointsY[0][1]
     top = pointsY[-1][1]
-    bounding_box = [left,right,bottom,top]
-    #print(bounding_box," \n",points )
-    return bounding_box, points
+    bounding_box = [left, right, bottom, top]
+    return bounding_box
 
-def split_points(points,box,quadrant):
+def split_points(features,mid_x, mid_y):
     """
-    :param points: vstupní seznam bodů
-    :param box: seznam obsahující souřadnice bodů left,right,bottom,up kvadrantu, pro který funkci počítáme
-    :param quadrant: číslo kvadrantu, pro který počítáme
-    :return: seznam
+    Funkce vytvoří seznam pro každý kvadrant, body podle x,y souřadnic rozdělí do jednotlivých kvadrantů a za cluster_id
+    každého bodu přiřadí 1 - 4 podle kvadrantu, do kterého spadá (pokud prvek ještě nemá cluster_id vytvoří jej)
+    :param features: vstupní seznam prvků vzniklých z geoJSON
+    :param mid_x: střed na ose x, podle kterého jsou prvky rozděleny
+    :param mid_y: střed na ose y, podle kterého jsou prvky rozděleny
+    :return: seznamy prvků náležících do jednotlivých kvadrantů - quad1, quad2, quad3, quad4
     """
-    quad = []
-    for point in points:
-        id = point[2]
-        id_cluster = id + str(quadrant)
-        x = point[0]
-        y = point[1]
-        if box[0] <= x <= box[2] and box[1] <= y <= box[3]:
-            quad.append([x,y,id_cluster])
-        else:
-            continue
-    #print(quad)
-    return quad
 
-def building_quadtree (features, box,quadrant= 0):
-    if len(features)< 5:
-        output_list = []
+    quad1 = []
+    quad2 = []
+    quad3 = []
+    quad4 = []
+    for feature in features:
+        if 'cluster_id' not in feature['properties']:
+            feature['properties']['cluster_id'] = ''
+        coords = feature['geometry']['coordinates']
+        x = coords[0]
+        y = coords[1]
+        if x <= mid_x and y > mid_y:
+            feature['properties']['cluster_id'] = str(feature['properties']['cluster_id']) + '1'
+            quad1.append(feature)
+        elif x > mid_x and y >= mid_y:
+            feature['properties']['cluster_id'] = str(feature['properties']['cluster_id']) + '2'
+            quad2.append(feature)
+        elif x < mid_x and y <= mid_y:
+            feature['properties']['cluster_id'] = str(feature['properties']['cluster_id']) + '3'
+            quad3.append(feature)
+        elif x >= mid_x and y < mid_y:
+            feature['properties']['cluster_id'] = str(feature['properties']['cluster_id']) + '4'
+            quad4.append(feature)
+    return quad1, quad2, quad3, quad4
+
+def quadtree (features, output_list, mid_x, mid_y, len_x, len_y, x = 0):
+    """
+    Funkce, mění hodnoty geometrického středu podle zadaného kvadrantu, pro který počítá, volá funkci split_features a
+    následně je rekurzivně volaná na každý kvadrant dokud není splněna podmínka velikosti vstupního souboru
+    :param features: vstupní seznam prvků
+    :param output_list: výstupní seznam prvků
+    :param mid_x: střed na ose x, dle kterého jsou rozděleny prvky
+    :param mid_y: střed na ose y, dle kterého jsou rozděleny prvky
+    :param len_x: vzdálenost na ose x, která odpovídá polovině vzdálenosti mezi krajními body x nadřazeného kvadrantu
+    :param len_y: vzdálenost na ose y, která odpovídá polovině vzdálenosti mezi krajními body y nadřazeného kvadrantu
+    :param x: číslo počítaného kvadrantu, defaultně nastaven na 0
+    :return: výstupní output_list
+    """
+    if len(features) < 50:
         for point in features:
             output_list.append(point)
-        print(output_list)
+        return output_list
 
-    else:
-        mid = [((box[0] + box[1]) / 2),((box[2] + box[3]) / 2)]
-        if quadrant == 0:
-            box = box
-        elif quadrant == 1:
-            box = [box[0],mid[0],mid[1],box[2]]
-        elif quadrant == 2:
-            box = [mid[0],box[1],mid[1],box[3]]
-        elif quadrant == 3:
-            box = [box[0],mid[0],box[2],mid[1]]
-        elif quadrant == 4:
-            box = [mid[0],box[1],box[2],mid[1]]
+    if x != 0:
+        if x == 1:
+            mid_x = mid_x - len_x
+            mid_y = mid_y + len_y
+        if x == 2:
+            mid_x = mid_x + len_x
+            mid_y = mid_y + len_y
+        if x == 3:
+            mid_x = mid_x - len_x
+            mid_y = mid_y - len_y
+        if x == 4:
+            mid_x = mid_x + len_x
+            mid_y = mid_y - len_y
 
-        quad1 = split_points(features,box,1)
-        quad2 = split_points(features,box,2)
-        quad3 = split_points(features,box,3)
-        quad4 = split_points(features,box,4)
-
-        # rekurzivní volání funkce v každém bounding boxu
-        building_quadtree(quad1,box,1)
-        building_quadtree(quad2,box,2)
-        building_quadtree(quad3,box,3)
-        building_quadtree(quad4,box,4)
-
-
-
-
-
-
-
+    quad1, quad2, quad3, quad4 = split_points(features, mid_x, mid_y)
+    quadtree(quad1, output_list, mid_x, mid_y, len_x / 2, len_y / 2, x=1)
+    quadtree(quad2, output_list, mid_x, mid_y, len_x / 2, len_y / 2, x=2)
+    quadtree(quad3, output_list, mid_x, mid_y, len_x / 2, len_y / 2, x=3)
+    quadtree(quad4, output_list, mid_x, mid_y, len_x / 2, len_y / 2, x=4)
+    # rekurzivní volání funkce v každém bounding boxu
+    return output_list
--- du2/split_zk.py
+++ du2/split_zk.py
@@ -1,11 +1,25 @@
 import json, quad_tree
 
 # načtení dat geojson ze složky a uložení do proměnné
-with open ("input.geojson","r",encoding="utf-8") as f:
+with open("mrizka3.json","r",encoding="utf-8") as f:
     data = json.load(f)
 features = data["features"]
-points = features[:10]
 
-bounding_box, points = quad_tree.edges(points)
-quad_tree.building_quadtree(points,bounding_box)
+print(len(features))
 
+box = quad_tree.edges(features)
+mid_x = ((box[0] + box[1])/2)
+mid_y = ((box[2] + box[3])/2)
+len_x = abs(box[0] - box[1])/2
+len_y = abs(box[2] - box[3])/2
+output_list = []
+output_list = quad_tree.quadtree(features,output_list,mid_x,mid_y,len_x,len_y)
+print(output_list)
+print("delka:", len(output_list))
+
+gj_structure = {"type": "FeatureCollection"}
+gj_structure['features'] = output_list
+
+# zapis souboru
+with open("mrizka3_output2.geojson", "w", encoding="utf-8") as f:
+    json.dump(gj_structure, f, indent=2)
