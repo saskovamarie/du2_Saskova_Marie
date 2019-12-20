ac7d5bde6d0553863d3c4fe6457a8aa6322789bf
saskovamarie <57861267+saskovamarie@users.noreply.github.com>
2019-12-20 15:27:01 +0100

    update split.py

--- du2/split.py
+++ du2/split.py
@@ -1,25 +1,26 @@
 import json, quad_tree
 
 # načtení dat geojson ze složky a uložení do proměnné
-with open("mrizka3.json","r",encoding="utf-8") as f:
+with open("input.geojson","r",encoding="utf-8") as f:
     data = json.load(f)
 features = data["features"]
 
-print(len(features))
+#print(len(features))
 
-box = quad_tree.edges(features)
-mid_x = ((box[0] + box[1])/2)
-mid_y = ((box[2] + box[3])/2)
-len_x = abs(box[0] - box[1])/2
-len_y = abs(box[2] - box[3])/2
+left,right,bottom,top = quad_tree.get_bounding_box(features)
+mid_x = ((left + right)/2)
+mid_y = ((bottom + top])/2)
+len_x = abs(left - right)/2
+len_y = abs(bottom - top)/2
 output_list = []
 output_list = quad_tree.quadtree(features,output_list,mid_x,mid_y,len_x,len_y)
-print(output_list)
-print("delka:", len(output_list))
+
+#  print(output_list)
+#  print("delka:", len(output_list))
 
 gj_structure = {"type": "FeatureCollection"}
 gj_structure['features'] = output_list
 
 # zapis souboru
-with open("mrizka3_output2.geojson", "w", encoding="utf-8") as f:
+with open("output.geojson", "w", encoding="utf-8") as f:
     json.dump(gj_structure, f, indent=2)
